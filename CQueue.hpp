49f6c4ffadc6022dff7c15254382b58cbc83001a
Calum Davison Bowden <calumalexad@gmail.com>
2020-02-17 18:49:14 +0000

    Implemented locking queue and some very basic testing

--- CQueue.hpp
+++ CQueue.hpp
@@ -2,6 +2,8 @@
 #define CQUEUE_H
 #include "QueueNode.hpp"
 #include <atomic>
+#include <mutex>
+#include <iostream>
 
 using namespace std;
 
@@ -18,11 +20,14 @@ class CQueue {
 
 template <typename T>
 class CQueue_L : public CQueue<T> {
+    private:
+        mutex qLock;
     public:
         void enqueue(T payload) override {
             Node<T>* newNode = new Node<T>(payload);
 
             //Critical section
+            qLock.lock();
             if(this->head) {
                 this->tail->setNext(newNode);
                 this->tail = newNode;
@@ -31,6 +36,8 @@ class CQueue_L : public CQueue<T> {
                 this->head = newNode;
                 this->tail = newNode;
             }
+            cout << "Added " << payload << " to queue\n";
+            qLock.unlock();
             //End of critical section
         };
 
@@ -39,9 +46,11 @@ class CQueue_L : public CQueue<T> {
             Node<T>* oldHead;
 
             //Critical section
-            if(!this->head) {
-                //Probably shouldn't be undefined, but eh, exceptions
-                return this->head->getVal();
+            qLock.lock();
+            if(!(this->head)) {
+                //cout << "Nothing in queue\n";
+                qLock.unlock();
+                throw 1;
             }
             else {
                 oldHead = this->head;
@@ -52,6 +61,8 @@ class CQueue_L : public CQueue<T> {
                     this->tail = nullptr;
                 }
             }
+            cout << "Removed " << returnVal << " from queue\n";
+            qLock.unlock();
             //End of critical section
 
             return returnVal; 
@@ -61,7 +72,7 @@ class CQueue_L : public CQueue<T> {
 template <typename T>
 class CQueue_LF : public CQueue<T> {
     void enqueue(T payload) override {
-
+        
     };
 
     T dequeue() override {
--- CQueueTests.cpp
+++ CQueueTests.cpp
@@ -1,11 +1,60 @@
 #include "CQueue.hpp"
 #include <iostream>
+#include <stdlib.h>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
-int main() {
-    CQueue_L<int> lockQueue;
-    CQueue_LF<int> lockFreeQueue;
-    lockQueue.enqueue(1);
-    cout << lockQueue.dequeue();
+CQueue_L<int> lockQueue;
+CQueue_LF<int> lockFreeQueue;
+
+void randomThreadSleep() {
+    int randSleep = rand() % 1000;
+    std::this_thread::sleep_for(std::chrono::milliseconds(randSleep));
+}
+
+void addToQueueL(int value) {
+    lockQueue.enqueue(value);
+    return;
+}
+
+void dequeueL(int id) {
+    randomThreadSleep();
+    int val = 0;
+    int count = 0;
+    bool removed = false;
+    while(!removed) {
+        try {
+            val = lockQueue.dequeue();
+            cout << "Tried: " << count << " times before removing " << val << "\n";
+            removed = true;
+        }
+        catch (int e) {
+            count += 1;
+            //Nothing in queue, try again
+        }
+    }
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    int maxThreads = stoi(argv[1]);
+    thread threads[maxThreads * 2];
+
+    if(argc == 3) {
+        srand(stoi(argv[2]));
+    }
+
+    for(int i = 0; i < maxThreads; i++) {
+        threads[i] = thread(addToQueueL, i);
+        threads[i + maxThreads] = thread(dequeueL, i + maxThreads);
+    }
+
+    for(int i = 0; i < maxThreads; i++) {
+        threads[i].join();
+        threads[i + maxThreads].join();
+    }
+
+    return 0;
 }
